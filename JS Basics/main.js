94eb8c20367cdfc88bf879937303ec9e6de0dd42
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2022-12-27 17:52:27 +0530

    js basics

--- JS Basics/main.js
+++ JS Basics/main.js
@@ -1,3 +1,9 @@
+// simple print statement -- single line comments
+console.log("Hello World!")
+
+/*Using Prompt 
+ And Alert -- multi line comments*/
+
 var Fname = prompt("Please enter your name: ");
 console.log(`Hello ${Fname}`);
 
@@ -6,3 +12,15 @@ if(age>=18)
     alert("Eligible")
 else
    alert("not Eligible")
+
+let a=10; //setting 'a' value to 10
+console.log(a);
+/*the value 10 is stored in 
+variable 'a' */
+
+//Random Numbers
+console.log(Math.random());
+console.log(Math.random()*100);
+console.log(Math.floor(Math.random()*100)); 
+//round number to nearest integer after multiplying it with 100
+
